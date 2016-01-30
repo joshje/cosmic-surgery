9163a32c6fbbac1b6be77eea70f185a353c1b793
Josh Emerson <josh.j.emerson@gmail.com>
2016-01-30 15:49:19 +0000

    Redirect to HTTPS

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 
 var app = express();
+
 app.set('port', (process.env.PORT || 3000));
+app.set('environment', (process.env.NODE_ENV || 'development'));
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+
+app.use(function(req, res, next) {
+  if (app.get('environment') !== 'development' && req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+
+  return next();
+});
 
 app.use(express.static(__dirname + '/dist'));
 
