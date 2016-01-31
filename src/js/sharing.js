b9e33d0f9554a0a1fc0ee51e6ef08a8c7aa5ca92
Josh Emerson <josh.j.emerson@gmail.com>
2016-01-31 18:45:39 +0000

    Updated sharing URLs

--- src/js/sharing.js
+++ src/js/sharing.js
@@ -1,15 +1,17 @@
 var renderer = require('./renderer');
 
 var shareLocations = {
-  facebook: 'https://www.facebook.com/dialog/share?app_id=1659003591054240&display=popup&href={{url}}&redirect_uri={{url}}',
-  twitter: 'http://www.twitter.com/share?url={{url}}'
+  facebook: 'https://www.facebook.com/dialog/share?app_id=1659003591054240&display=popup&href={{url}}&redirect_uri={{url}}&caption={{text}}',
+  twitter: 'http://www.twitter.com/share?text={{text}}&url={{url}}&hashtags=cosmicsurgery'
 };
 
 var share = function(evt) {
   var type = evt.target.getAttribute('data-type');
 
   renderer.getImageUrl(function(data) {
-    var shareLocation = shareLocations[type].replace(/{{url}}/g, encodeURIComponent('https://cosmic-surgery.herokuapp.com?share=' + data.id));
+    var shareLocation = shareLocations[type]
+    .replace(/{{url}}/g, encodeURIComponent('https://cosmic-surgery.herokuapp.com?share=' + data.id))
+    .replace(/{{text}}/g, encodeURIComponent('Give yourself a cosmic facelift'));
     window.location = shareLocation;
   });
 };
