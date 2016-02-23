b870c0ad5da487a760033859f906d374044605f7
Josh Emerson <josh.j.emerson@gmail.com>
2016-02-23 21:05:28 +0000

    Add icosahedron paths

--- src/js/origami-renderer.js
+++ src/js/origami-renderer.js
@@ -18,14 +18,23 @@ var drawImage = function(source, ctx, image, path) {
 
   scratchCtx.clearRect(0, 0, scratchCanvas.width, scratchCanvas.height);
 
-  var sx = image[0];
-  var sy = image[1];
-  var sw = image[2];
-  var sh = image[3];
-  var dw = path.destination[2];
-  var dh = path.destination[3];
-  var dtx = path.destination[0] + dw * 0.5;
-  var dty = path.destination[1] + dh * 0.5;
+  var pathXs = path.points.map(function(point) { return point[0]; });
+  var pathYs = path.points.map(function(point) { return point[1]; });
+  var minX = Math.min.apply(Math, pathXs);
+  var maxX = Math.max.apply(Math, pathXs);
+  var minY = Math.min.apply(Math, pathYs);
+  var maxY = Math.max.apply(Math, pathYs);
+  var width = maxX - minX;
+  var height = maxY - minY;
+
+
+  var sx = image[0] - 100;
+  var sy = image[1] - 100;
+  var sw = 200;
+  var sh = 200;
+
+  var dtx = minX + width * 0.5;
+  var dty = minY + height * 0.5;
 
   scratchCtx.translate(dtx, dty);
 
@@ -36,15 +45,13 @@ var drawImage = function(source, ctx, image, path) {
     scratchCtx.globalAlpha = 0.5;
   }
 
-  scratchCtx.drawImage(source.el, (source.width - cw) / 2 + sx, (source.height - ch) / 2 + sy, sw, sh, dw * -0.5, dh * -0.5, dw, dh);
+  scratchCtx.drawImage(source.el, (source.width - cw) / 2 + sx, (source.height - ch) / 2 + sy, sw, sh, sw * -0.5, sh * -0.5, sw, sh);
 
   scratchCtx.restore();
   scratchCtx.save();
 
   scratchCtx.fillStyle = '#fff';
-  if (! window.debug) {
-    scratchCtx.globalCompositeOperation = 'destination-in';
-  }
+  scratchCtx.globalCompositeOperation = 'destination-in';
   scratchCtx.beginPath();
 
   scratchCtx.moveTo(path.points[0][0], path.points[0][1]);
@@ -74,7 +81,7 @@ var renderFrame = function(source, ctx, width, height) {
   if (window.debug) {
     for (var j = 0; j < type.images.length; j++) {
       var image = type.images[j];
-      ctx.strokeRect(720 - image[0] - image[2], image[1], image[2], image[3]);
+      ctx.strokeRect(720 - image[0] - 100, image[1] - 100, 200, 200);
     }
   }
 
@@ -82,6 +89,9 @@ var renderFrame = function(source, ctx, width, height) {
     var path = type.paths[i];
     drawImage(source, ctx, type.images[path.image], path);
   }
+
+  var shadowImg = document.querySelector('[data-shadow=' + currentType + ']');
+  ctx.drawImage(shadowImg, 0, 0, cw, ch);
 };
 
 var changeType = function(type) {
