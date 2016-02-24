4a1a24a918082f9406f77e4a5d140a176559827a
Josh Emerson <josh.j.emerson@gmail.com>
2016-02-24 21:16:02 +0000

    Update sharing flow

--- src/js/sharing.js
+++ src/js/sharing.js
@@ -1,9 +1,10 @@
 var gator = require('gator');
 var stateManager = require('./state-manager');
 var renderer = require('./renderer');
-var buttonsTmpl = require('../../views/partials/shareButtons.handlebars');
-var waitTmpl = require('../../views/partials/shareWait.handlebars');
-var chooseTmpl = require('../../views/partials/shareChoose.handlebars');
+
+var captureTmpl = require('../../views/partials/share/capture.handlebars');
+var waitTmpl = require('../../views/partials/share/wait.handlebars');
+var shareTmpl = require('../../views/partials/share/share.handlebars');
 
 var shareUrl;
 
@@ -16,48 +17,51 @@ var shareLocations = {
   twitter: 'http://www.twitter.com/share?text={{text}}&url={{url}}&hashtags=cosmicsurgery'
 };
 
-var share = function() {
-  var type = this.getAttribute('data-share');
+var capture = function() {
   render(waitTmpl);
 
   renderer.getImageUrl(function(data) {
     shareUrl = 'https://cosmic-surgery.herokuapp.com?share=' + data.id;
 
-    render(chooseTmpl, {
-      type: type,
-      isDownload: type == 'download',
+    render(shareTmpl, {
       downloadUrl: data.url
     });
   });
+
+  return false;
 };
 
-var choose = function() {
-  var type = this.getAttribute('data-choose');
+var share = function() {
+  var type = this.getAttribute('data-share');
 
-  if (type == 'retry') {
-    render(buttonsTmpl);
-    renderer.render();
-    // retry
-  } else {
-    var shareLocation = shareLocations[type]
-    .replace(/{{url}}/g, encodeURIComponent(shareUrl))
-    .replace(/{{text}}/g, encodeURIComponent('I gave myself a cosmic facelift!'));
+  var shareLocation = shareLocations[type]
+  .replace(/{{url}}/g, encodeURIComponent(shareUrl))
+  .replace(/{{text}}/g, encodeURIComponent('I gave myself a cosmic facelift!'));
 
-    var sw = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    var sh = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  var sw = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var sh = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-    var w = 600;
-    var h = 400;
-    var left = (sw - w) / 2;
-    var top = (sh - h) / 2;
+  var w = 600;
+  var h = 400;
+  var left = (sw - w) / 2;
+  var top = (sh - h) / 2;
 
-    window.open(shareLocation, 'cosmic-share', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=' + w + ',height=' + h + ',left=' + left + ',top=' + top);
-  }
+  window.open(shareLocation, 'cosmic-share', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=' + w + ',height=' + h + ',left=' + left + ',top=' + top);
+
+  return false;
+};
+
+var retry = function() {
+  render(captureTmpl);
+  renderer.render();
+
+  return false;
 };
 
 var init = function() {
+  gator(document).on('click', '[data-capture]', capture);
   gator(document).on('click', '[data-share]', share);
-  gator(document).on('click', '[data-choose]', choose);
+  gator(document).on('click', '[data-retry]', retry);
 };
 
 module.exports = {
