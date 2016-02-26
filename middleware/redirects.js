84ac9673a564e77d94c465cb3a80164e78a1fca5
Josh Emerson <josh.j.emerson@gmail.com>
2016-02-26 19:06:40 +0000

    Redirect from www domain

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -1,6 +1,10 @@
 module.exports = function(req, res, next) {
-  if (req.protocol !== 'https') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  var host = req.get('Host');
+
+  if (host.match(/^www./i)) {
+    return res.redirect(['https://', host.replace('www.', ''), req.url].join(''));
+  } else if (req.protocol !== 'https') {
+    return res.redirect(['https://', host, req.url].join(''));
   }
 
   return next();
